12ceb7229c38887fe1b376293c6b0b05471ff56b
alisafa60 <alisafa6100@gmail.com>
2023-12-28 22:37:17 +0200

    seeding implementation plus user types initialization

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,57 @@
+// Import Prisma Client
+import { PrismaClient, Type as UserTypeType } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const userTypesData = [
+  { type: 'admin' as UserTypeType },
+  { type: 'user' as UserTypeType },
+  { type: 'healthProfessional' as UserTypeType },
+];
+
+
+async function seedUserTypes() {
+  try {
+    await Promise.all(
+      userTypesData.map(async (userData) => {
+        await prisma.userType.create({
+          data: {
+            type: userData.type,
+          },
+        });
+      })
+    );
+
+    console.log('UserType data seeded successfully');
+  } catch (error) {
+    console.error('Error seeding UserType data:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+seedUserTypes();
+
+
+// async function main() {
+//     const user = await prisma.user.create({
+//         data:{
+//             email:'ali@hotmail.com',
+//             firstName:'ali',
+//             lastName:'safa',
+//             gender:'male',
+//             password:'ali1234',
+//             unit:'Metric',
+//             userTypeId:2,
+//         }
+//     })
+// }
+
+// main()
+//     .catch((e:Error)=>{
+//         console.error(e);
+//         process.exit(1);
+//     })
+//     .finally(async () =>{
+//         await prisma.$disconnect();
+//     })
