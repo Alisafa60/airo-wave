2b7827e2c49ced4bb976b6a922966c16a11e58b8
alisafa60 <alisafa6100@gmail.com>
2023-12-30 17:53:03 +0200

    modifications to separate concerns

--- backend/src/controllers/healthControllers/allergy.controllers.js
+++ backend/src/controllers/healthControllers/allergy.controllers.js
@@ -1,45 +1,28 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const medicationController = require('./medication.controllers');
 
 const addAllergy = async (req, res) => {
     try {
-      const { allergen, severity, duration, triggers, medicationFields } = req.body;
+      const { allergen, severity, duration, triggers } = req.body;
   
-      // Extract user ID from the JWT (assuming it's included in req.user)
       const userId = req.user.id;
   
-      // Query the database to find the userHealthId based on the userId
       const userHealth = await prisma.healthCondition.findUnique({
         where: { userId: userId },
       });
-  
-      // Ensure that userHealth is not null and userHealthId is present
+
       if (!userHealth || !userHealth.id) {
         return res.status(400).json({ error: 'User health information not found.' });
       }
   
-      // Use the obtained userHealthId in the allergy creation
       const userHealthId = userHealth.id;
   
-      let newMedication = null;
-  
-      // Check if medicationFields are provided
-      if (medicationFields) {
-        // Use the medication controller to create or find the medication
-        newMedication = await medicationController.createOrFindMedication(medicationFields);
-      }
-  
-      // Create the allergy in the database
       const newAllergy = await prisma.allergy.create({
         data: {
           allergen,
           severity,
           duration,
           triggers,
-          medications: newMedication
-            ? { connect: { id: newMedication.id } }
-            : undefined,
         },
       });
   
@@ -58,68 +41,7 @@ const addAllergy = async (req, res) => {
     }
 };
 
-const findExistingAllergy = async ({ allergen, severity, duration, triggers }) => {
-    const existingAllergy = await prisma.allergy.findUnique({
-      where: {
-        allergen,
-        severity,
-        duration,
-        triggers,
-      },
-    });
-  
-    return existingAllergy;
-  };
-  
+
   module.exports = { 
     addAllergy,
-    findExistingAllergy
  };
-
-// module.exports = { addAllergy };
-
-// const addAllergy = async (req, res) => {
-//   try {
-//     const userId = req.user.id;
-//     const { allergen, severity, duration, triggers, medicationFields } = req.body;
-
-//     // Check if medicationFields are provided
-//     const newMedication = medicationFields
-//       ? await medicationController.createOrFindMedication(medicationFields)
-//       : null;
-
-//     // Check if an existing allergy with the same details exists
-//     const existingAllergy = await prisma.allergy.findFirst({
-//       where: {
-//         allergen,
-//         severity,
-//         duration,
-//         triggers,
-//       },
-//     });
-
-//     if (existingAllergy) {
-//       // If the allergy already exists, return it
-//       return res.status(200).json({ allergy: existingAllergy });
-//     }
-
-//     // If the allergy doesn't exist, create a new one
-//     const newAllergy = await prisma.allergy.create({
-//       data: {
-//         allergen,
-//         severity,
-//         duration,
-//         triggers,
-//         medications: newMedication
-//           ? { connect: { id: newMedication.id } }
-//           : undefined,
-//       },
-//     });
-
-//     res.status(201).json({ allergy: newAllergy });
-//   } catch (e) {
-//     console.error("Error adding allergy", e);
-//     res.status(500).json({ error: e.message });
-//   }
-// };
-
--- backend/src/controllers/healthControllers/userHealth.controllers.js
+++ backend/src/controllers/healthControllers/userHealth.controllers.js
@@ -1,24 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const allergyController = require('./allergy.controllers');
-const respiratoryConditionController = require('./respiratoryCondition.controllers');
 
 const addUserHealth = async (req, res) => {
   try {
     const userId = req.user.id;
-    const { weight, bloodType, respiratoryConditionFields, allergyFields } = req.body;
-
-    const newRespiratoryCondition = respiratoryConditionFields
-      ? await respiratoryConditionController.addRespiratoryCondition(req, res)
-      : null;
-
-    let existingAllergy;
-
-    // Check if allergyFields are provided
-    if (allergyFields) {
-      // If allergyFields are provided, try to find an existing allergy
-      existingAllergy = await allergyController.findExistingAllergy(allergyFields);
-    }
+    const { weight, bloodType } = req.body;
 
     const newUserHealth = await prisma.healthCondition.create({
       data: {
@@ -29,27 +15,16 @@ const addUserHealth = async (req, res) => {
             id: userId,
           },
         },
-        respiratoryCondition: newRespiratoryCondition
-          ? { connect: { id: newRespiratoryCondition.id } }
-          : undefined,
-        allergy: existingAllergy
-          ? { connect: { id: existingAllergy.id } }
-          : undefined,
       },
     });
 
-    res.status(201).json({ userHealth: newUserHealth });
+    const userHealthId = newUserHealth.id;
+
+    res.status(201).json({ userHealth: newUserHealth, userHealthId });
   } catch (e) {
     console.error('Error adding new user health', e);
-
-    // Check if the error is specifically related to the missing allergy
-    if (e.message.includes('allergy')) {
-      res.status(400).json({ error: 'Allergy is required for user health entry.' });
-    } else {
-      res.status(500).json({ error: e.message });
-    }
+    res.status(500).json({ error: e.message });
   }
 };
 
 module.exports = { addUserHealth };
-
