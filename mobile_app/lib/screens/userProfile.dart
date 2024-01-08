6ad8e24e05dfda52934a21e58752801c6311d3e2
alisafa60 <alisafa6100@gmail.com>
2024-01-09 00:14:57 +0200

    <mobile_app> adjusted user profile screen

--- mobile_app/lib/screens/userProfile.dart
+++ mobile_app/lib/screens/userProfile.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:popup_menu/popup_menu.dart';
 
-class UserProfileScreen extends StatelessWidget {
-   UserProfileScreen({Key? key}) : super(key: key);
+class UserProfileScreen extends StatefulWidget {
+  UserProfileScreen({Key? key}) : super(key: key);
 
+  @override
+  _UserProfileScreenState createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
   final GlobalKey _containerKey = GlobalKey();
+  bool _isMetric = true; 
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(255, 252, 252, 1),
         actions: [
@@ -29,6 +37,7 @@ class UserProfileScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Align(
               alignment: Alignment.center,
@@ -53,7 +62,186 @@ class UserProfileScreen extends StatelessWidget {
                   ),
                 ),
               ),
-            )
+            ),
+            const SizedBox(height: 50,),
+            Container(
+              height: 40,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              child: const TextField(
+                textAlign: TextAlign.start,
+                textAlignVertical: TextAlignVertical.bottom,
+                decoration: InputDecoration(
+                  hintText: ' First name',
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(74, 74, 74, 0.4),
+                  ),
+                  border: InputBorder.none,
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Color.fromRGBO(255, 115 , 29, 0.6)), 
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Color.fromRGBO(74, 74, 74, 0.4)), 
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Container(
+              height: 40,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              child: const TextField(
+                textAlign: TextAlign.start,
+                textAlignVertical: TextAlignVertical.bottom,
+                decoration: InputDecoration(
+                  hintText: ' Last name',
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(74, 74, 74, 0.4),
+                  ),
+                  border: InputBorder.none,
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Color.fromRGBO(255, 115 , 29, 0.6)), 
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Color.fromRGBO(74, 74, 74, 0.4)), 
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Container(
+              height: 40,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              child: const TextField(
+                textAlign: TextAlign.start,
+                textAlignVertical: TextAlignVertical.bottom,
+                decoration: InputDecoration(
+                  hintText: ' Phone number',
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(74, 74, 74, 0.4),
+                  ),
+                  border: InputBorder.none,
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Color.fromRGBO(255, 115 , 29, 0.6)), 
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Color.fromRGBO(74, 74, 74, 0.4)), 
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Container(
+              height: 40,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              child: DropdownButtonFormField<String>(
+                decoration: const InputDecoration(
+                  hintText: 'Select gender',
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(74, 74, 74, 0.4),
+                  ),
+                  border: InputBorder.none,
+                    focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Color.fromRGBO(255, 115 , 29, 0.6)), 
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Color.fromRGBO(74, 74, 74, 0.4)), 
+                  ),
+                ),
+                items: ['Male', 'Female', 'Other'].map((String gender) {
+                  return DropdownMenuItem<String>(
+                    value: gender,
+                    child: Text(gender),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  
+                },
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Container(
+              height: 40,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              child: const TextField(
+                textAlign: TextAlign.start,
+                textAlignVertical: TextAlignVertical.bottom,
+                decoration: InputDecoration(
+                  hintText: ' Address',
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(74, 74, 74, 0.4),
+                  ),
+                  border: InputBorder.none,
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Color.fromRGBO(255, 115 , 29, 0.6)), 
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Color.fromRGBO(74, 74, 74, 0.4)), 
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  ' Preferred Units',
+                  style: TextStyle(
+                    color: Color.fromRGBO(74, 74, 74, 1),
+                    fontSize: 16,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      _isMetric ? 'Metric' : 'Imperial',
+                      style: TextStyle(
+                        color: Color.fromRGBO(74, 74, 74, 0.8),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Switch(
+                      value: _isMetric,
+                      onChanged: (value) {
+                        setState(() {
+                          _isMetric = value;
+                        });
+                      },
+                      activeColor: Color.fromRGBO(255, 115, 29, 0.6),
+                      inactiveThumbColor: Color.fromRGBO(95, 157, 247, 0.8),
+                      activeTrackColor: Color.fromRGBO(255, 115, 29, 0.2),
+                      inactiveTrackColor: Color.fromRGBO(95, 157, 247, 0.4),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 30,),
+             Container(
+              height: 50,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(255, 115, 19, 1),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const Center(
+               child: Text(
+                'Save',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+               )
+              ),
+            ),
           ],
         ),
       ),
@@ -83,5 +271,6 @@ class UserProfileScreen extends StatelessWidget {
       },
     );
     menu.show(widgetKey: _containerKey);
+
   }
 }
