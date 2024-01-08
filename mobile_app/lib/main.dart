42e8c4f236842b1a74b64d7d455075e4f6ef10e5
alisafa60 <alisafa6100@gmail.com>
2024-01-08 04:44:59 +0200

    <mobile-app> completed sign up screen

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './screens/home.dart';
+import './screens/signup.dart';
 
 
 void main() {
@@ -33,7 +34,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      // home: const HomeScreen(),
+      home: const SignupScreen(),
     );
   }
 }
---
+++ mobile_app/lib/screens/signup.dart
@@ -0,0 +1,213 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget{
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:const Color.fromRGBO(255, 252, 252, 1),
+      ),
+      
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Create an Account",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+                color: Color.fromRGBO(46, 46, 46, 1),
+              ),
+            ),
+            const SizedBox(height: 40),
+            const Text(
+              'Email',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+                color: Color.fromRGBO(74, 74, 74, 1),
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Container(
+              height: 50,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color.fromRGBO(74, 74, 74, 0.5)),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const TextField(
+                textAlign: TextAlign.start,
+                textAlignVertical: TextAlignVertical.bottom,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(74, 74, 74, 0.4),
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Passwrod',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+                color: Color.fromRGBO(74, 74, 74, 1),
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Container(
+              height: 50,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color.fromRGBO(74, 74, 74, 0.5)),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const TextField(
+                textAlign: TextAlign.start,
+                textAlignVertical: TextAlignVertical.bottom,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(74, 74, 74, 0.4),
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 5,),
+            const Text(
+              'Password must contain at least 8 characters',
+              style: TextStyle(
+                color: Color.fromRGBO(74, 74, 74, 0.6),
+                fontSize: 15,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              height: 50,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(255, 115, 19, 1),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const Center(
+               child: Text(
+                'Sign Up',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+               )
+              ),
+            ),
+            const SizedBox(height: 15),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 1,
+                    color: Colors.grey,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    'or',
+                    style: TextStyle(
+                      color: Color.fromRGBO(74, 74, 74, 0.6),
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 1,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 15,),
+            Container(
+              height: 50,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 247, 241),
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: const Color.fromRGBO(74, 74, 74, 0.5)),
+              ),
+              child: const Center(
+               child: Text(
+                'Continue with Google',
+                style: TextStyle(
+                  color: Color.fromRGBO(74, 74, 74, 1),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+               )
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Container(
+              height: 50,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 247, 241),
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: const Color.fromRGBO(74, 74, 74, 0.5)),
+              ),
+              child: const Center(
+               child: Text(
+                'Continue with Facebook',
+                style: TextStyle(
+                  color: Color.fromRGBO(74, 74, 74, 1),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+               )
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Container(
+              height: 50,
+              width: double.infinity,
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 247, 241),
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: const Color.fromRGBO(74, 74, 74, 0.5)),
+              ),
+              child: const Center(
+               child: Text(
+                'Continue with Apple',
+                style: TextStyle(
+                  color: Color.fromRGBO(74, 74, 74, 1),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+               )
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
+ 
