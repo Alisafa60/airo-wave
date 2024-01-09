a6000dfec324596c307cca88fe3d3ec6265abf58
alisafa60 <alisafa6100@gmail.com>
2024-01-09 21:21:38 +0200

    <mobile_app> adjusted user health screen

--- mobile_app/lib/screens/user_health.dart
+++ mobile_app/lib/screens/user_health.dart
@@ -29,8 +29,11 @@ class _UserHealthState extends State<UserHealth> {
     return validBloodTypes.contains(bloodType.toUpperCase());
   }
 
+  List<bool> _isEditing = [];
+
   @override
   Widget build(BuildContext context) {
+    _isEditing = List.generate(_selectedConditions.length, (index) => false);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(255, 252, 252, 1),
@@ -146,87 +149,65 @@ class _UserHealthState extends State<UserHealth> {
                 expandedHeaderPadding: EdgeInsets.all(0),
                 expansionCallback: (int index, bool isExpanded) {
                   setState(() {
-                    _selectedConditions[index] = isExpanded ? 'None' : _selectedConditions[index];
+                    _isEditing[index] = false;
+                    _selectedConditions[index] =
+                        isExpanded ? 'None' : _selectedConditions[index];
                   });
                 },
                 children: _selectedConditions.asMap().entries.map((entry) {
                   int index = entry.key;
                   String condition = entry.value;
+                  bool isEditing = _isEditing[index];
+
                   return ExpansionPanel(
                     headerBuilder: (BuildContext context, bool isExpanded) {
-                      return InkWell(
-                        onTap: () {
-                          setState(() {
-                            _selectedConditions[index] = !isExpanded ? 'None' : _selectedConditions[index];
-                          });
-                        },
-                        child: Container(
-                          height: 50,
-                          width: 100,
-                          padding: const EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: const Color.fromRGBO(74, 74, 74, 0.5)),
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          child: Row(
-                            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              DropdownButton<String>(
-                                value: condition,
-                                onChanged: (value) {
-                                  setState(() {
-                                    _selectedConditions[index] = value!;
-                                    _updateFieldControllers(index);
-                                  });
-                                },
-                                items: _conditionItems,
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.delete),
-                                onPressed: () {
-                                  setState(() {
-                                    _selectedConditions.removeAt(index);
-                                    _fieldControllers.removeAt(index);
-                                  });
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
+                      return _buildConditionHeader(index, condition, _isEditing[index]);
                     },
                     body: Column(
                       children: [
-                        if (condition != 'None')
+                        if (_isEditing[index] || condition != 'None')
                           ..._buildFieldsForCondition(condition, index),
                         const SizedBox(height: 5),
-                        InkWell(
-                          onTap: () {
-                            setState(() {
-                              _fieldControllers[index].add(TextEditingController());
-                            });
-                          },
-                          child: Container(
-                            height: 50,
-                            width: 50,
-                            padding: const EdgeInsets.all(5),
-                            margin: const EdgeInsets.symmetric(vertical: 5),
-                            decoration: BoxDecoration(
-                              border: Border.all(color: const Color.fromRGBO(74, 74, 74, 0.5)),
-                              borderRadius: BorderRadius.circular(5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.save),
+                              onPressed: () {
+                                // Handle save logic here
+                                setState(() {
+                                  _isEditing[index] = false;
+                                  _selectedConditions[index] =
+                                      _selectedConditions[index] != 'None'
+                                          ? _selectedConditions[index]
+                                          : _conditionItems.first.value!;
+                                });
+                              },
                             ),
-                          ),
+                            IconButton(
+                              icon: Icon(Icons.edit),
+                              onPressed: () {
+                                // Handle edit logic here
+                                setState(() {
+                                  _isEditing[index] = true;
+                                  _selectedConditions[index] = condition;
+                                });
+                              },
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                    isExpanded: condition != 'None',
+                    isExpanded: _isEditing[index] || condition != 'None',
                   );
                 }).toList(),
               ),
+
               const SizedBox(height: 5),
               InkWell(
                 onTap: () {
                   setState(() {
+                    _isEditing.add(false);
                     _selectedConditions.add('None');
                     _fieldControllers.add([TextEditingController()]);
                   });
@@ -256,26 +237,33 @@ class _UserHealthState extends State<UserHealth> {
   }
 
   List<Widget> _buildFieldsForCondition(String condition, int index) {
+    List<Widget> fields = [];
+
     switch (condition) {
       case 'Allergy':
-        return [
+        fields = [
           _buildTextField('Allergen', index, 0),
           _buildTextField('Severity', index, 1),
           _buildTextField('Duration', index, 2),
           _buildTextField('Triggers', index, 3),
         ];
+        break;
       case 'Respiratory Condition':
-        return [
+        fields = [
           _buildTextField('Condition', index, 0),
           _buildTextField('Diagnosis', index, 1),
           _buildTextField('Symptoms Frequency', index, 2),
           _buildTextField('Triggers', index, 3),
         ];
+        break;
       case 'Other':
-        return [_buildTextField('Other', index, 0)];
+        fields = [_buildTextField('Other', index, 0)];
+        break;
       default:
-        return [];
+        break;
     }
+
+    return fields;
   }
 
   Widget _buildTextField(String hintText, int conditionIndex, int fieldIndex) {
@@ -306,9 +294,118 @@ class _UserHealthState extends State<UserHealth> {
     );
   }
 
-  void _updateFieldControllers(int index) {
-    _fieldControllers[index].forEach((controller) {
-      controller.clear();
-    });
+  Widget _buildConditionHeader(int index, String condition, bool isEditing) {
+    if (isEditing) {
+      // Render editable field for the first condition entered
+      
+      if (_fieldControllers[index].isNotEmpty) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                // Handle save logic here
+                setState(() {
+                  _isEditing[index] = false;
+                  _selectedConditions[index] = 'None';
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                // Handle edit logic here
+                setState(() {
+                  _isEditing[index] = true;
+                });
+              },
+            ),
+          ],
+        );
+      } else {
+        return Container(); // No fields to display
+      }
+    } else {
+      return InkWell(
+        onTap: () {
+          setState(() {
+            _isEditing[index] = false;
+            _selectedConditions[index] =
+                _selectedConditions[index] == 'None' ? condition : 'None';
+          });
+        },
+        child: Container(
+          height: 50,
+          width: 100,
+          padding: const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            border: Border.all(color: const Color.fromRGBO(74, 74, 74, 0.5)),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: _fieldControllers[index].isEmpty
+              ? Row(
+                  children: [
+                    DropdownButton<String>(
+                      value: condition,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedConditions[index] = value!;
+                          _updateFieldControllers(index);
+                        });
+                      },
+                      items: _conditionItems,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        setState(() {
+                          _selectedConditions.removeAt(index);
+                          _fieldControllers.removeAt(index);
+                        });
+                      },
+                    ),
+                  ],
+                )
+              : Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _fieldControllers[index][0],
+                        decoration: InputDecoration(
+                          hintText: condition,
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.save),
+                      onPressed: () {
+                        // Handle save logic for editable field
+                        setState(() {
+                          _isEditing[index] = false;
+                          _selectedConditions[index] = condition;
+                        });
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () {
+                        // Handle edit logic for editable field
+                        setState(() {
+                          _isEditing[index] = true;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+        ),
+      );
+    }
   }
+  void _updateFieldControllers(int index) {
+    if (_fieldControllers[index].isEmpty) {
+    _fieldControllers[index].add(TextEditingController());
+  }
+}
 }
