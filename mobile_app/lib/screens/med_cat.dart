3bdc35b261c69424ed4604d8f0637019ad6e4a3b
alisafa60 <alisafa6100@gmail.com>
2024-01-12 23:03:37 +0200

    <mobile_app> added expandable list

--- mobile_app/lib/screens/med_cat.dart
+++ mobile_app/lib/screens/med_cat.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:sliding_up_panel2/sliding_up_panel2.dart';
 
 class MedCatScreen extends StatefulWidget {
   const MedCatScreen({Key? key}) : super(key: key);
@@ -10,6 +9,8 @@ class MedCatScreen extends StatefulWidget {
 }
 
 class _MedCatScreenState extends State<MedCatScreen> {
+  bool isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,185 +32,262 @@ class _MedCatScreenState extends State<MedCatScreen> {
           ),
         ),
       ),
-      body: SlidingUpPanel(
-        maxHeight: MediaQuery.of(context).size.height * 0.7,
-        minHeight: 80,
-        panel: _buildPanel(),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  color: const Color.fromRGBO(255, 252, 252, 1),
-                  child: Column(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          //route for user profile
-                        },
-                        child: ClipOval(
-                          child: Container(
-                            width: 60,
-                            height: 60,
-                            decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage('lib/assets/images/profile-picture.png'),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 5,),
-                      const Text(
-                          'Ali Safa',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                            color: Color.fromRGBO(74, 74, 74, 1),
-                          )
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20,),
-                Text(
-                  ' How is your Allergy today?',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Color.fromRGBO(74, 74, 74, 1),
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Container(
-                  child: Container(
-                    height: 55,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black12,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        for (int i = 1; i <= 5; i++)
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      color: const Color.fromRGBO(255, 252, 252, 1),
+                      child: Column(
+                        children: [
                           GestureDetector(
                             onTap: () {
-                              print('Pressed button $i');
+                              //route for user profile
                             },
-                            child: CircleAvatar(
-                              backgroundColor: const Color.fromRGBO(255, 117, 19, 0.683),
-                              child: Text(
-                                i.toString(),
-                                style: const TextStyle(
-                                  color: Colors.white,
+                            child: ClipOval(
+                              child: Container(
+                                width: 60,
+                                height: 60,
+                                decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage('lib/assets/images/profile-picture.png'),
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                      ],
+                          const SizedBox(height: 5,),
+                          const Text(
+                            'Ali Safa',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: Color.fromRGBO(74, 74, 74, 1),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 15,),
-                Container(
-                  height: 130,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                      color: Colors.black12,
-                      width: 2,
+                    const SizedBox(height: 20,),
+                    Text(
+                      ' How is your Allergy today?',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color.fromRGBO(74, 74, 74, 1),
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
-                  ),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: 0,
-                        right: 0,
-                        child: GestureDetector(
-                          onTap: () {
-                            // Handle tap
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(5),
-                            decoration: const BoxDecoration(
-                              color: Color.fromRGBO(255, 117, 19, 1),
-                              borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(10),
-                                bottomLeft: Radius.circular(10),
+                    SizedBox(height: 10,),
+                    Container(
+                      child: Container(
+                        height: 55,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black12,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            for (int i = 1; i <= 5; i++)
+                              GestureDetector(
+                                onTap: () {
+                                  print('Pressed button $i');
+                                },
+                                child: CircleAvatar(
+                                  backgroundColor: const Color.fromRGBO(255, 117, 19, 0.683),
+                                  child: Text(
+                                    i.toString(),
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15,),
+                    Container(
+                      height: 130,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: Colors.black12,
+                          width: 2,
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child: GestureDetector(
+                              onTap: () {
+                                // Handle tap
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(5),
+                                decoration: const BoxDecoration(
+                                  color: Color.fromRGBO(255, 117, 19, 1),
+                                  borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(10),
+                                    bottomLeft: Radius.circular(10),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Show Analysis',
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    color: Colors.white,
+                                  ),
+                                ),
                               ),
                             ),
-                            child: const Text(
-                              'Show Analysis',
-                              style: TextStyle(
-                                fontSize: 10,
-                                color: Colors.white,
-                              ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isExpanded = !isExpanded;
+              });
+            },
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 500),
+              height: isExpanded ? MediaQuery.of(context).size.height * 0.67 : 200,
+              width: 400,
+              decoration: BoxDecoration(
+                 color:  Color.fromARGB(56, 6, 6, 6),
+                 borderRadius: BorderRadius.circular(10),
+              ),
+             
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 10,),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isExpanded = true;
+                        });
+                        print('Container pressed!');
+                      },
+                      child: Container(
+                        width: 300,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color.fromRGBO(95, 156, 247, 0.653),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Press me!',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    
+                    SizedBox(height: 10,),
+                   GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isExpanded = true;
+                        });
+                        print('Container pressed!');
+                      },
+                      child: Container(
+                        width: 300,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color.fromRGBO(95, 156, 247, 0.653),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Press me!',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 10,),
-              ],
+              )
             ),
           ),
-        ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset('lib/assets/icons/home-filled-gray.svg',
-                height: 35, width: 35,),
-              label: 'Home',
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'lib/assets/icons/home-filled-gray.svg',
+              height: 35,
+              width: 35,
             ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset('lib/assets/icons/MedCat-orange.svg',
-                height: 35, width: 35,),
-              label: 'MedCat',
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset('lib/assets/icons/map-location.svg',
-                height: 35, width: 35,),
-              label: 'Maps',
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset('lib/assets/icons/activity-waves.svg',
-                height: 35, width: 35,),
-              label: 'Activities',
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset('lib/assets/icons/community.svg',
-                height: 35, width: 35,),
-              label: 'Community',
-            ),
-          ]
-      ),
-    );
-  }
- 
-   Widget _buildPanel() {
-    // Customize your panel content here
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text(
-          'Panel Content',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+            label: 'Home',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'lib/assets/icons/MedCat-orange.svg',
+              height: 35,
+              width: 35,
+            ),
+            label: 'MedCat',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'lib/assets/icons/map-location.svg',
+              height: 35,
+              width: 35,
+            ),
+            label: 'Maps',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'lib/assets/icons/activity-waves.svg',
+              height: 35,
+              width: 35,
+            ),
+            label: 'Activities',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'lib/assets/icons/community.svg',
+              height: 35,
+              width: 35,
+            ),
+            label: 'Community',
+          ),
+        ],
       ),
     );
   }
