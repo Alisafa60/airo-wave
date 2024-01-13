1a681e30979c4780918d6255babe31a58c1f94c9
alisafa60 <alisafa6100@gmail.com>
2024-01-14 00:48:00 +0200

    <mobile_app> updated constants

--- mobile_app/lib/constants.dart
+++ mobile_app/lib/constants.dart
@@ -11,46 +11,63 @@ const Color myGray = Color.fromRGBO(74, 74, 74, 1);
 class BorderedInputField extends StatelessWidget {
   final String hintText;
   final IconData? customIcon;
+  final String errorText;
 
   const BorderedInputField({
-    super.key,
+    Key? key,
     required this.hintText,
     this.customIcon,
-  });
+    this.errorText = "",
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 50,
       width: double.infinity,
+      // padding: const EdgeInsets.all(5),
+      // decoration: BoxDecoration(
+      //   border: Border.all(
+      //     color: errorText.isNotEmpty ? Colors.red : Color.fromRGBO(74, 74, 74, 0.5),
+      //   ),
+      //   borderRadius: BorderRadius.circular(5),
+      // ),
       child: TextField(
         textAlign: TextAlign.start,
         textAlignVertical: TextAlignVertical.bottom,
+        obscureText: hintText.toLowerCase().contains('password'),
         decoration: InputDecoration(
           hintText: hintText,
           hintStyle: TextStyle(
-            color: myGray.withOpacity(0.4),
+            color: Color.fromRGBO(74, 74, 74, 0.4),
           ),
           prefixIcon: customIcon != null
               ? Icon(
                   customIcon,
-                  color: myGray.withOpacity(0.7),
+                  color: Color.fromRGBO(74, 74, 74, 0.7),
                 )
               : null,
           border: InputBorder.none,
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(7),
-            borderSide: BorderSide(width: 2, color: primaryColor.withOpacity(0.6)),
+            borderSide: BorderSide(
+              width: 2,
+              color: errorText.isNotEmpty ? Colors.red : primaryColor.withOpacity(0.6),
+            ),
           ),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(7),
-            borderSide: BorderSide(width: 2, color: myGray.withOpacity(0.2)),
+            borderSide: BorderSide(
+              width: 2,
+              color: errorText.isNotEmpty ? Colors.red : myGray.withOpacity(0.2),
+            ),
           ),
         ),
       ),
     );
   }
 }
+
 
 class SaveButton extends StatelessWidget {
   final String buttonText;
