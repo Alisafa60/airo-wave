336b3be56c9c301058d44e598ae59a959bd028a2
alisafa60 <alisafa6100@gmail.com>
2024-01-14 19:08:06 +0200

    <mobile_app> handling photo upload api

--- mobile_app/lib/api_survice.dart
+++ mobile_app/lib/api_survice.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:mobile_app/constants.dart';
+import 'package:http_parser/http_parser.dart';
+
 
 class ApiService {
   final String baseUrl;
@@ -9,17 +10,41 @@ class ApiService {
 
   Future<http.Response> post(String path, Map<String, String> body) async {
     final Uri uri = Uri.parse('$baseUrl$path');
-
+    
     try {
       final http.Response response = await http.post(
         uri,
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode(body),
       );
-
       return response;
     } catch (error) {
       throw Exception('Error during API call: $error');
     }
   }
+  
+   Future<http.Response> postMultipart(String path, Map<String, String> headers, Map<String, dynamic> body) async {
+  final Uri uri = Uri.parse('$baseUrl$path');
+
+  final http.MultipartRequest request = http.MultipartRequest('POST', uri)
+    ..headers.addAll(headers);
+
+  // Add each part of the body to the request
+  for (MapEntry<String, dynamic> entry in body.entries) {
+    if (entry.value is String) {
+      request.fields[entry.key] = entry.value;
+    } else if (entry.value is http.MultipartFile) {
+      request.files.add(entry.value);
+    }
+  }
+
+  try {
+    final http.Response response = await http.Response.fromStream(await request.send());
+    return response;
+  } catch (error) {
+    throw Exception('Error during API call: $error');
+  }
 }
+
+}
+
