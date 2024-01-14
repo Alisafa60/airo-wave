1b23a032a539aaa9dd9ab110d172da268a8149a3
alisafa60 <alisafa6100@gmail.com>
2024-01-14 23:49:02 +0200

    <backend> corrected routes

--- backend/src/routes/image.routes.js
+++ backend/src/routes/image.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { uploadProfilePicture, deleteProfilePicture } = require('../controllers/imageControllers/image.controllers');
 const upload = require('../utils/imageUpload');
 
-router.post('/users/:userId/profile-picture', upload.single('profilePicture'), uploadProfilePicture);
-router.delete('/users/:userId/profile-picture', deleteProfilePicture);
+router.post('/user/profile-picture', upload.single('profilePicture'), uploadProfilePicture);
+router.delete('/user/profile-picture', deleteProfilePicture);
 
 module.exports = router;
