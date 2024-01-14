9376aa89cf94e1add7f1d850747e33cc388bef3c
alisafa60 <alisafa6100@gmail.com>
2024-01-14 23:44:44 +0200

    <backend> corrected routes in index.js>

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const {authMiddleware} = require('./middlewares/auth.middleware');
 require("dotenv").config();
-
+const path = require('path');
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
 
+app.use('/uploads', express.static(path.join(__dirname, 'src/uploads')));
 const authRoutes = require('./routes/auth.routes');
 const imageRoutes = require('./routes/image.routes');
 const allergyRoutes = require('./routes/allergy.routes');
@@ -20,7 +21,7 @@ const dailyHealth = require('./routes/dailyHealth.routes');
 const enviromentalHealth = require('./routes/enviromentalHealth.routes');
 const notification = require('./routes/notification.routes');
 const healthProfessional = require('./routes/healthProfessional.routes');
-const userProfile = require('./routes/userHealth.routes')
+const userProfile = require('./routes/userProfile.routes')
 const machineLearningPredictions = require('./routes/ml.routes');
 const contactProfessional = require('./routes/contractProfessional.routes');
 const chatbot = require('./routes/chatbot.routes');
@@ -28,6 +29,7 @@ const heartRate = require('./routes/heartRate.routes');
 const stress = require('./routes/stress.routes');
 const userRoutes = require('./routes/createRoutes.controllers');
 const device = require('./routes/device.routes');
+
 
 app.use('/auth', authRoutes);
 app.use('/api', authMiddleware, imageRoutes);
@@ -41,18 +43,18 @@ app.use('/api', authMiddleware, dailyHealth );
 app.use('/api', authMiddleware, enviromentalHealth);
 app.use('/api', authMiddleware, notification);
 app.use('/api', authMiddleware, healthProfessional);
-app.use('api', authMiddleware, userProfile);
-app.use('api', authMiddleware, machineLearningPredictions);
+app.use('/api', authMiddleware, userProfile);
+app.use('/api', authMiddleware, machineLearningPredictions);
 app.use('/api', authMiddleware, contactProfessional);
 app.use('/api', authMiddleware, chatbot);
 app.use('/api', authMiddleware, stress);
 app.use('/api', authMiddleware, heartRate);
 app.use('/api', authMiddleware, userRoutes);
 app.use('/api', authMiddleware, device);
-
 module.exports = app;
 
 const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
