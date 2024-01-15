e9dc060b03b9c44ca29d8ecb0731a25c50ffaff3
alisafa60 <alisafa6100@gmail.com>
2024-01-16 01:20:24 +0200

    <mobile_app> adjusted positiong and styling in record activity

--- mobile_app/lib/screens/record_activity.dart
+++ mobile_app/lib/screens/record_activity.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mobile_app/constants.dart';
 
-class RecordActivityScreen extends StatelessWidget {
+class RecordActivityScreen extends StatefulWidget {
   const RecordActivityScreen({super.key});
+  @override
+  _RecordActivityScreen createState() => _RecordActivityScreen();
+}
+
+class _RecordActivityScreen extends State<RecordActivityScreen> {
+  double startButtonScale = 1.0;
 
   @override
   Widget build(BuildContext context) {
@@ -33,90 +39,109 @@ class RecordActivityScreen extends StatelessWidget {
       body: Stack(
         children: [
           
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 40,
-                        decoration: BoxDecoration(
-                          border: Border(
-                            right: BorderSide(
-                              color: myGray.withOpacity(0.2),
-                              width: 2,
+         Align(
+            alignment: Alignment.bottomCenter,
+            child: FractionallySizedBox(
+              heightFactor: 0.25,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            border: Border(
+                              right: BorderSide(
+                                color: myGray.withOpacity(0.2),
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                          child: Center(
+                            child: SvgPicture.asset(
+                              'lib/assets/icons/environment.svg',
+                              height: 40,
+                              width: 40,
                             ),
                           ),
                         ),
-                        child: Center(
-                          child: SvgPicture.asset(
-                            'lib/assets/icons/environment.svg',
-                            height: 40,
-                            width: 40,
-                          ),
-                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        height: 40,
-                        decoration: BoxDecoration(
-                          border: Border(
-                            right: BorderSide(
-                              color: myGray.withOpacity(0.2),
-                              width: 2,
+                      Expanded(
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            border: Border(
+                              right: BorderSide(
+                                color: myGray.withOpacity(0.2),
+                                width: 2,
+                              ),
                             ),
                           ),
-                        ),
-                        child: const Center(
-                          child: Icon(Icons.run_circle_outlined,
-                              color: myGray, size: 30),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        height: 40,
-                        decoration: BoxDecoration(
-                          border: Border(
-                            right: BorderSide(
-                              color: myGray.withOpacity(0.2),
-                              width: 2,
-                            ),
+                          child: Center(
+                            child: Icon(Icons.run_circle_outlined,
+                                color: myGray.withOpacity(0.6), size: 30),
                           ),
                         ),
-                        child: const Center(
-                          child: Icon(Icons.monitor_heart,
-                              color: myGray, size: 30),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            border: Border(
+                              right: BorderSide(
+                                color: myGray.withOpacity(0.2),
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                          child: Center(
+                            child: Icon(Icons.monitor_heart,
+                                color: myGray.withOpacity(0.6), size: 30),
+                          ),
                         ),
                       ),
-                    ),
-                    Expanded(
+                      Expanded(
+                        child: Container(
+                          height: 40,
+                          child: Center(
+                            child: Icon(Icons.route_outlined,
+                                color: myGray.withOpacity(0.6), size: 30),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                    onTapDown: (_) {
+                      setState(() {
+                        startButtonScale = 0.9;
+                      });
+                    },
+                    onTapUp: (_) {
+                      setState(() {
+                        startButtonScale = 1.0;
+                      });
+                      // Handle the button press action here
+                    },
+                    child: Transform.scale(
+                      scale: startButtonScale,
                       child: Container(
-                        height: 40,
-                        child: const Center(
-                          child: Icon(Icons.route_outlined,
-                              color: myGray, size: 30),
+                        height: 70,
+                        width: 70,
+                        child: Image.asset(
+                          'lib/assets/icons/start-button.png',
+                          height: 70,
                         ),
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Container(
-                  height: 70,
-                  width: 100,
-                  child: Image.asset('lib/assets/icons/start-button.png',
-                      height: 70),
-                ),
-                SizedBox(height: 20),
-              ],
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
             ),
           ),
         ],
