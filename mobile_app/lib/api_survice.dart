944d048b7dd2f59dd435512bacb5cc7c4da8ae5c
alisafa60 <alisafa6100@gmail.com>
2024-01-16 15:58:07 +0200

    <mobile_app> added api helpers

--- mobile_app/lib/api_survice.dart
+++ mobile_app/lib/api_survice.dart
@@ -6,13 +6,28 @@ class ApiService {
 
   ApiService(this.baseUrl);
 
-  Future<http.Response> post(String path, Map<String, String> body) async {
+  Future<http.Response> post1(String path, Map<String, String> body) async {
     final Uri uri = Uri.parse('$baseUrl$path');
     
     try {
       final http.Response response = await http.post(
         uri,
         headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(body),
+      );
+      return response;
+    } catch (error) {
+      throw Exception('Error during API call: $error');
+    }
+  }
+  
+  Future<http.Response> post(String path, Map<String, String> headers, Map<String, dynamic> body) async {
+    final Uri uri = Uri.parse('$baseUrl$path');
+
+    try {
+      final http.Response response = await http.post(
+        uri,
+        headers: headers,
         body: jsonEncode(body),
       );
       return response;
