e8227f3f3f4d096c716a860f464f4936937c2cda
alisafa60 <alisafa6100@gmail.com>
2024-01-18 00:53:49 +0200

    <mobile_app> changed styling on health data screen

--- mobile_app/lib/screens/health_data.dart
+++ mobile_app/lib/screens/health_data.dart
@@ -2,36 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:mobile_app/constants.dart';
 
-class ShowHealthScreen  extends StatelessWidget{
+class ShowHealthScreen extends StatefulWidget {
   const ShowHealthScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  State<ShowHealthScreen> createState() => _ShowHealthScreenState();
+}
 
+class _ShowHealthScreenState extends State<ShowHealthScreen> {
+  int selectedContainerIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar:   
-        AppBar(
-        backgroundColor:const Color.fromRGBO(255, 252, 252, 1),
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(255, 252, 252, 1),
         title: const Text(
           "Health",
           selectionColor: Color.fromRGBO(74, 74, 74, 1),
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.w500,
-        ),),
+          ),
+        ),
         centerTitle: true,
-        leading: IconButton (
+        leading: IconButton(
           padding: EdgeInsets.all(15),
           icon: Icon(Icons.arrow_back_ios, color: primaryColor,),
-          onPressed: () {
-          },),
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(1),
-            child: Container(
-              height: 1,
-              color: Colors.black12,
-            ),
+          onPressed: () {},
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1),
+          child: Container(
+            height: 1,
+            color: Colors.black12,
+          ),
         ),
       ),
       body: Padding(
@@ -70,88 +76,152 @@ class ShowHealthScreen  extends StatelessWidget{
                       color: Color.fromRGBO(74, 74, 74, 1),
                     ),
                   ),
-                ]
-              )
+                ],
+              ),
             ),
             Row(
               children: [
                 Expanded(
-                  child: Container(
-                    height: 110,
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(74, 74, 74, 0.2),
-                      borderRadius: BorderRadius.circular(15), 
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedContainerIndex = 0;
+                      });
+                    },
+                    child: Container(
+                      height: 110,
+                      decoration: BoxDecoration(
+                        color: myGray.withOpacity(0.2),
+                        border: Border.all(
+                          color: selectedContainerIndex == 0
+                              ? primaryColor
+                              : Colors.transparent,
+                        ),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
                     ),
                   ),
                 ),
                 const SizedBox(width: 10),
                 Expanded(
-                  child: Container(
-                    height: 110,
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(74, 74, 74, 0.2),
-                      borderRadius: BorderRadius.circular(15), 
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedContainerIndex = 1;
+                      });
+                    },
+                    child: Container(
+                      height: 110,
+                      decoration: BoxDecoration(
+                        color: myGray.withOpacity(0.2),
+                        border: Border.all(
+                          color: selectedContainerIndex == 1
+                              ? primaryColor 
+                              : Colors.transparent,
+                        ),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
                     ),
                   ),
-                )
+                ),
               ],
             ),
             SizedBox(height: 10,),
-            Container(
-              height: 120,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(74, 74, 74, 0.2),
-                borderRadius: BorderRadius.circular(15), 
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedContainerIndex = 2;
+                });
+              },
+              child: Container(
+                height: 120,
+                
+                decoration: BoxDecoration(
+                  color: myGray.withOpacity(0.2),
+                  border: Border.all(
+                    color: selectedContainerIndex == 2
+                        ? primaryColor 
+                        : Colors.transparent,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
               ),
             ),
             SizedBox(height: 10,),
-             Container(
-              height: 120,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(74, 74, 74, 0.2),
-                borderRadius: BorderRadius.circular(15), 
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedContainerIndex = 3;
+                });
+              },
+              child: Container(
+                height: 120,
+                
+                decoration: BoxDecoration(
+                  color: myGray.withOpacity(0.2),
+                  border: Border.all(
+                    color: selectedContainerIndex == 3
+                        ? primaryColor
+                        : Colors.transparent,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
               ),
             ),
             SizedBox(height: 10,),
-             Container(
-              height: 120,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(74, 74, 74, 0.2),
-                borderRadius: BorderRadius.circular(15), 
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedContainerIndex = 4;
+                });
+              },
+              child: Container(
+                height: 120,
+                
+                decoration: BoxDecoration(
+                  color: myGray.withOpacity(0.2),
+                  border: Border.all(
+                    color: selectedContainerIndex == 4
+                        ? primaryColor 
+                        : Colors.transparent,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
               ),
             ),
           ],
         ),
       ),
-       bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
             icon: SvgPicture.asset('lib/assets/icons/home-filled.svg',
-            height: 35, width: 35,),
+              height: 35, width: 35,),
             label: 'Home',
           ),
           BottomNavigationBarItem(
             icon: SvgPicture.asset('lib/assets/icons/MedCat.svg',
-            height: 35, width: 35,),
+              height: 35, width: 35,),
             label: 'MedCat',
           ),
           BottomNavigationBarItem(
             icon: SvgPicture.asset('lib/assets/icons/map-location.svg',
-            height: 35, width: 35,),
+              height: 35, width: 35,),
             label: 'Maps',
           ),
           BottomNavigationBarItem(
             icon: SvgPicture.asset('lib/assets/icons/activity-waves.svg',
-            height: 35, width: 35,),
+              height: 35, width: 35,),
             label: 'Activities',
           ),
           BottomNavigationBarItem(
             icon: SvgPicture.asset('lib/assets/icons/community.svg',
-            height: 35, width: 35,),
+              height: 35, width: 35,),
             label: 'Community',
           ),
-        ]),
+        ],
+      ),
     );
   }
 }
