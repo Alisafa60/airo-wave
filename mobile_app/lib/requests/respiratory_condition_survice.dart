23ffda3e3317427358ed7a39e13a8aed001f3252
alisafa60 <alisafa6100@gmail.com>
2024-01-18 04:25:13 +0200

    <mobile_app> added respiratory condition  survice class

---
+++ mobile_app/lib/requests/respiratory_condition_survice.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:mobile_app/api_survice.dart';
+
+class RespiratoryConditionSurvice {
+  final ApiService apiService;
+  RespiratoryConditionSurvice(this.apiService);
+  Future<Map<String, dynamic>> getRespiratoryCondition() async {
+     String? token = await _getToken();
+
+    if (token != null) {
+      final Map<String, String> headers = {'Authorization': 'Bearer $token'};
+
+      try {
+        final http.Response response = await apiService.get(
+          '/api/user/health',
+          headers,
+        );
+
+        if (response.statusCode == 200) {
+          final Map<String, dynamic> data = json.decode(response.body);
+          return data;
+        } else {
+          throw Exception('Failed to load user health data. Status code: ${response.statusCode}, Body: ${response.body}');
+        }
+      } catch (error) {
+        throw Exception('Error during health data API call: $error');
+      }
+    } else {
+      throw Exception('Token is null. Unable to make the API request.');
+    }
+  }
+
+  static Future<String?> _getToken() async {
+    const FlutterSecureStorage storage = FlutterSecureStorage();
+    return await storage.read(key: 'jwtToken');
+  }
+}
