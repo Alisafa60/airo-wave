dfc33ab6baa5359f663a5eb48bdb202a0368bad2
alisafa60 <alisafa6100@gmail.com>
2024-01-18 11:18:47 +0200

    <backend> adjusted respiratory condition get route

--- backend/src/routes/respiratoryCondition.routes.js
+++ backend/src/routes/respiratoryCondition.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const respiratoryConditionController = require('../controllers/healthControllers/respiratoryCondition.controllers');
 
 router.post('/user/health/respiratoryCondition', respiratoryConditionController.addRespiratoryCondition);
-router.get('/user/health/healespiratoryConditions', respiratoryConditionController.getAllRespiratoryConditions);
+router.get('/user/health/respiratoryConditions', respiratoryConditionController.getAllRespiratoryConditions);
 router.get('/user/health/respiratoryCondition/:id', respiratoryConditionController.getRespiratoryConditionById);
 router.put('/user/health/respiratoryCondition/:id', respiratoryConditionController.updateRespiratoryConditionById);
 router.delete('/user/health/respiratoryCondition/:id', respiratoryConditionController.deleteRespiratoryConditionById);
