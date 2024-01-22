03e4d9b736642ebece9c1987559164faf373d55d
alisafa60 <alisafa6100@gmail.com>
2024-01-22 21:56:49 +0200

    <backend> added get last enviromental data controller

--- backend/src/controllers/enviromentalHealthControllers/enviromentalHealth.controllers.js
+++ backend/src/controllers/enviromentalHealthControllers/enviromentalHealth.controllers.js
@@ -2,6 +2,7 @@ const { PrismaClient } = require('@prisma/client');
 const Joi = require('joi');
 const prisma = new PrismaClient();
 const { handleError } = require('../../helpers');
+const prisma1 = require('../../../dist/sql/userLocation');
 
 const environmentalDataSchema = Joi.object({
   temperature: Joi.number().allow(null),
@@ -10,6 +11,8 @@ const environmentalDataSchema = Joi.object({
   treePollen: Joi.number().allow(null),
   grassPollen: Joi.number().allow(null),
   weedPollen: Joi.number().allow(null),
+  olivePollen: Joi.number().allow(null),
+  birchPollen: Joi.number().allow(null),
   co2Level: Joi.number().allow(null),
   ozoneLevel: Joi.number().allow(null),
   coLevel: Joi.number().allow(null),
@@ -17,8 +20,9 @@ const environmentalDataSchema = Joi.object({
   so2Level: Joi.number().allow(null),
   no2Level: Joi.number().allow(null),
   pm1_0: Joi.number().allow(null),
-  pm2_5: Joi.number().allow(null),
+  pm25: Joi.number().allow(null),
   pm10: Joi.number().allow(null),
+  dominantPollutant: Joi.string().allow(null),
   windSpeed: Joi.number().allow(null),
   bloodO2Level: Joi.string().allow(null),
   heartRate: Joi.array().items(Joi.object({
@@ -26,16 +30,19 @@ const environmentalDataSchema = Joi.object({
     timestamp: Joi.date().required(),
   })).optional(),
   deviceId: Joi.number().optional(),
-  userLocationId: Joi.number().required(),
   userId: Joi.number().required(),
+  location: Joi.object({
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
+  }).required(),
 });
 
 const createEnvironmentalData = async (req, res) => {
   try {
-    const { temperature, humidity, aqi, treePollen, grassPollen, weedPollen, co2Level, ozoneLevel, coLevel, vocLevel, so2Level, no2Level, pm1_0, pm2_5, pm10, windSpeed, bloodO2Level, heartRate, deviceId, userLocationId} = req.body;
+    const { temperature, humidity, aqi, treePollen, grassPollen, weedPollen, olivePollen, birchPollen, co2Level, ozoneLevel, coLevel, vocLevel, so2Level, no2Level, pm1_0, pm25, pm10, windSpeed, bloodO2Level, dominantPollutant, heartRate, deviceId, location} = req.body;
     const userId = req.user.id;
     const { error, value } = environmentalDataSchema.validate({
-      temperature, humidity, aqi, treePollen, grassPollen, weedPollen, co2Level, ozoneLevel, coLevel, vocLevel, so2Level, no2Level, pm1_0, pm2_5, pm10, windSpeed, bloodO2Level, heartRate, deviceId, userLocationId, userId
+      temperature, humidity, aqi, treePollen, grassPollen, weedPollen, olivePollen, birchPollen, co2Level, ozoneLevel, coLevel, vocLevel, so2Level, no2Level, pm1_0, pm25, pm10, windSpeed, bloodO2Level, dominantPollutant, heartRate, deviceId, userId, location
     });
 
     if (error) {
@@ -51,6 +58,25 @@ const createEnvironmentalData = async (req, res) => {
     handleError(res, e, 'Error creating EnvironmentalHealthData');
   }
 };
+
+const getLastEnvironmentalData = async (req, res) => {
+  try{
+    const userId = req.user.id;
+
+    getLastEnvironmentalData = await prisma.enviromentalHealthData.findFirst({
+      where: { userId: userId},
+      orderBy:{ createdAt: 'desc'}
+    })
+
+    if (!getLastEnvironmentalData){
+      return res.status(404).json({error: 'No Enviromental Data found'});
+    }
+    
+    res.status(200).json({getLastEnvironmentalData});
+  }catch(e){
+    handleError(res, e, 'Error retrieving EnvironmentalHealthData');
+  }
+}
 
 const getEnvironmentalDataByDateRange = async (req, res) => {
     try {
@@ -104,4 +130,5 @@ module.exports = {
   createEnvironmentalData,
   getEnvironmentalDataByDateRange,
   getEnvironmentalDataList,
+  getLastEnvironmentalData,
 }
