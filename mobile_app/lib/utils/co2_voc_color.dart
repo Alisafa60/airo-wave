bd723ef5dfb782e81f10f50f42d5eecaaa58aaa8
alisafa60 <alisafa6100@gmail.com>
2024-01-23 05:19:39 +0200

    <mobile_app> color change of environmental data

--- mobile_app/lib/utils/co2_voc_color.dart
+++ mobile_app/lib/utils/co2_voc_color.dart
@@ -9,8 +9,6 @@ Color getStatusColor(int value, String type) {
       return secondaryColor;
     } else if (value < 1400) {
       return primaryColor;
-    } else if (value < 2000) {
-      return Colors.red;
     } else {
       return Colors.red; 
     }
@@ -19,15 +17,81 @@ Color getStatusColor(int value, String type) {
       return secondaryColor;
     } else if (value < 660) {
       return primaryColor;
-    } else if (value < 1400) {
+    }else {
       return Colors.red;
-    } else if (value < 2000) {
-      return Colors.red;
+    }
+  } else if (type == 'aqi') {
+    if (value < 50) {
+      return secondaryColor;
+    } else if (value < 100) {
+      return Color.fromARGB(255, 255, 161, 53);
+    } else if (value < 150) {
+      return primaryColor;
     } else {
       return Colors.red;
     }
   } else {
     return Colors.black;
+  }
+}
+
+Color getColorEnv(double value, String type){
+  
+  if (type == 'co') {
+    if (value < 30) {
+      return secondaryColor;
+    } else if (value < 70) {
+      return Color.fromARGB(255, 255, 161, 53);
+    } else if (value < 150) {
+      return primaryColor;
+    } else {
+      return Colors.red;
+    }
+  } else if (type == 'so2') {
+    if (value < 0.1) {
+      return secondaryColor;
+    } else if (value < 0.2) {
+      return Color.fromARGB(255, 255, 161, 53);
+    } else if (value < 1) {
+      return primaryColor;
+    } else {
+      return Colors.red;
+    }
+  } else if (type == 'pm10') {
+    if (value < 54) {
+      return secondaryColor;
+    } else if (value < 150) {
+      return Color.fromARGB(255, 255, 161, 53);
+    } else if (value < 250) {
+      return primaryColor;
+    } else {
+      return Colors.red;
+    }
+  }else if (type=='no2'){
+    if (value < 54) {
+      return secondaryColor;
+    } else if (value < 100) {
+      return Color.fromARGB(255, 255, 161, 53);
+    } else if (value < 360) {
+      return primaryColor;
+    } else {
+      return Colors.red;
+    }
+  }else{
+    return Colors.black;
+  }
+}
+
+
+Color getColorForCO(double coLevel) {
+  if (coLevel < 30) {
+    return secondaryColor; 
+  } else if (coLevel < 70) {
+    return Color.fromARGB(255, 255, 161, 53); 
+  } else if (coLevel < 150) {
+    return primaryColor; 
+  } else {
+    return Colors.red; 
   }
 }
 
