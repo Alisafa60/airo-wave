da35c378fdf0d64252aacf4e57488f6a6abf975d
alisafa60 <alisafa6100@gmail.com>
2024-01-23 17:44:08 +0200

    <mobile_app> adjusted pollen model

--- mobile_app/lib/models/pollen.model.dart
+++ mobile_app/lib/models/pollen.model.dart
@@ -3,20 +3,23 @@ class Pollen {
   final int allergen2;
   final int allergen3;
   final int allergen4;
+  final int allergen5;
 
   Pollen({
     required this.allergen1,
     required this.allergen2,
     required this.allergen3,
     required this.allergen4,
+    required this.allergen5,
   });
 
   Map<String, dynamic> toJson() {
     return {
       'allergen1': allergen1,
       'allergen2': allergen2,
-      'symptomsFrequency': allergen3,
-      'triggers': allergen4,
+      'allergen3': allergen3,
+      'allergen4': allergen4,
+      'allergen5': allergen5,
     };
   }
 }
