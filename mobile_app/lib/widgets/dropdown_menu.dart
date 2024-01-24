c8be5e676543423393966ad687840f3d4a80c27f
alisafa60 <alisafa6100@gmail.com>
2024-01-24 17:10:17 +0200

    <mobile_app> resolving drop down menu dsiplay issue

--- mobile_app/lib/widgets/dropdown_menu.dart
+++ mobile_app/lib/widgets/dropdown_menu.dart
@@ -1,64 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app/constants.dart';
 
-class GenderDropdownFormField extends StatefulWidget {
+class GenderDropdownForm extends StatefulWidget {
   final String? currentValue;
   final ValueChanged<String?> onChanged;
-  const GenderDropdownFormField({super.key, required this.currentValue, required this.onChanged});
-  
+
+  const GenderDropdownForm({super.key, required this.currentValue, required this.onChanged});
+
   @override
-  State<GenderDropdownFormField> createState() => _MyWidgetState();
+  State<GenderDropdownForm> createState() => _MyWidgetState();
 }
 
-class _MyWidgetState extends State<GenderDropdownFormField> {
+class _MyWidgetState extends State<GenderDropdownForm> {
   String? currentValue;
+
   @override
   void initState() {
     currentValue = widget.currentValue;
-    // TODO: implement initState
     super.initState();
   }
-   @override
+
+  @override
   Widget build(BuildContext context) {
     print(currentValue);
     return Container(
       height: 40,
       width: double.infinity,
-      padding: const EdgeInsets.all(10),
-      child: 
-          DropdownButtonFormField<String>(
-            style: TextStyle(color: Colors.black, fontSize: 18),
-            selectedItemBuilder: (context) => [Text(currentValue!=null? currentValue! : '')],
-            decoration: InputDecoration(
-              hintText: 'Select gender',
-              hintStyle: TextStyle(
-                color: myGray.withOpacity(0.4),
-              ),
-              border: InputBorder.none,
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(width: 2, color: primaryColor.withOpacity(0.6)),
-              ),
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(width: 2, color: myGray.withOpacity(0.4)),
-              ),
+      padding: const EdgeInsets.all(7),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 2, color: myGray.withOpacity(0.4)),
+        ),
+      ),
+      child: InkWell(
+        onTap: () {
+          _showGenderPicker(context);
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              currentValue ?? 'Select gender',
+              style: TextStyle(color: myGray.withOpacity(0.7), fontSize: 16, fontWeight: FontWeight.w500),
             ),
-            value: currentValue,
-            items: ['Male', 'Female', 'Other'].map((String gender) {
-              return DropdownMenuItem<String>(
-                value: gender,
-                child: Text(gender),
+            Icon(Icons.arrow_drop_down, color: myGray.withOpacity(0.7),),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showGenderPicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          child: ListView(
+            shrinkWrap: true, 
+            children: ['Male', 'Female', 'Other'].map((String gender) {
+              return ListTile(
+                title: Text(gender),
+                onTap: () {
+                  setState(() {
+                    currentValue = gender;
+                  });
+                  widget.onChanged(gender);
+                  Navigator.pop(context);
+                },
               );
             }).toList(),
-            
-            onChanged: (value){
-              setState(() {
-                currentValue = value;
-              });
-              widget.onChanged(value);
-            }
           ),
-        
-      
+        );
+      },
     );
   }
 }
