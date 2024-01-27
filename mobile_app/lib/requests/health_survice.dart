12b2f572b64c3fcdb8c0b1e6e25d7ffb1a0cc403
alisafa60 <alisafa6100@gmail.com>
2024-01-27 10:09:53 +0200

    <mobile_app> added  weight post request

--- mobile_app/lib/requests/health_survice.dart
+++ mobile_app/lib/requests/health_survice.dart
@@ -36,4 +36,37 @@ class HealthService {
     const FlutterSecureStorage storage = FlutterSecureStorage();
     return await storage.read(key: 'jwtToken');
   }
+
+  Future<Map<int, dynamic>> updateWeight({
+    required weight,
+  }) async {
+    String? token = await _getToken();
+
+    if (token != null) {
+      final Map<String, String> headers = {'Authorization': 'Bearer $token', 'Content-Type': 'application/json'};
+      
+      try {
+        final Map<int, dynamic> requestBody = {
+          weight: int.parse(weight),
+        };
+
+        final http.Response response = await apiService.post(
+          '/api/user/health/edit',
+          headers,
+          requestBody.cast<String, dynamic>(),
+        );
+        print(requestBody);
+        if (response.statusCode == 200) {
+          final Map<int, dynamic> updateWeight = json.decode(response.body);
+          return updateWeight;
+        } else {
+          throw Exception('Failed to update medication. Status code: ${response.statusCode}, Body: ${response.body}, response: ${response}');
+        }
+      } catch (error) {
+        throw Exception('Error during medication update API call: $error');
+      }
+    } else {
+      throw Exception('Token is null. Unable to make the API request.');
+    }
+  }
 }
