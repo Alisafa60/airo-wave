9598488d8a09773ec88e76b5e6b46c85c999dca0
alisafa60 <alisafa6100@gmail.com>
2024-01-27 10:11:06 +0200

    <mobile_app> fetching profile data

---
+++ mobile_app/lib/requests/profile.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:mobile_app/api_service.dart';
+
+class ProfileService {
+  final ApiService apiService;
+  ProfileService(this.apiService);
+  Future<Map<String, dynamic>> getProfile() async {
+     String? token = await _getToken();
+
+    if (token != null) {
+      final Map<String, String> headers = {'Authorization': 'Bearer $token'};
+
+      try {
+        final http.Response response = await apiService.get(
+          '/api/user/profile',
+          headers,
+        );
+
+        if (response.statusCode == 200) {
+          final Map<String, dynamic> data = json.decode(response.body);
+          return data;
+        } else {
+          throw Exception('Failed to load profile. Status code: ${response.statusCode}, Body: ${response.body}');
+        }
+      } catch (error) {
+        throw Exception('Error during profile API call: $error');
+      }
+    } else {
+      throw Exception('Token is null. Unable to make the API request.');
+    }
+  }
+
+  static Future<String?> _getToken() async {
+    const FlutterSecureStorage storage = FlutterSecureStorage();
+    return await storage.read(key: 'jwtToken');
+  }
+}
