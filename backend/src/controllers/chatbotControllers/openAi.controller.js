ebb10678702130c48ba7336ca23c8f884c023a9e
alisafa60 <alisafa6100@gmail.com>
2024-01-28 21:57:48 +0200

    <backend> adding get response request

--- backend/src/controllers/chatbotControllers/openAi.controller.js
+++ backend/src/controllers/chatbotControllers/openAi.controller.js
@@ -1,13 +1,41 @@
 const chatBot = require('./chatbot');
+const handleError = require('../../helpers');
+const { PrismaClient } = require("@prisma/client");
+const { error } = require('console');
+const prisma = new PrismaClient();
 
-const charBotController = async(req, res) => {
+const chatBotController = async(req, res) => {
     try{
         const userId = req.user.id;
         const userMessage = req.body;
 
         const openAi = chatBot.generateOpenAIPayload(userId, userMessage);
-        
-    }catch(error){
 
+        res.status(201).json({ response: openAi });
+    }catch (e) {
+        handleError(res, e, 'Error adding allergen');
     }
 }
+
+const getLastResponse = async(req, res) => {
+    
+    try{
+        const userId = req.user.id;
+        
+        const openAiResponse = await prisma.openAiResponse.findFirst({
+            where : { userId: userId},
+            orderBy : {timestamp: 'desc'},
+        })
+
+        if (!openAiResponse){
+           return res.status(404).json({error: 'no response found'});
+        }
+        
+        res.status(200).json({openAiResponse});
+    }catch(e){
+        handleError(res, e, 'Error retrieving OpenAI response');
+        console.log(error);
+    }
+}
+
+module.exports = {chatBotController, getLastResponse};
