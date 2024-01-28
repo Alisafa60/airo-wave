c355251972c4b51c7f4ad1a9d2ca4738678ee6d7
alisafa60 <alisafa6100@gmail.com>
2024-01-28 04:29:45 +0200

    <mobile_app> chaning color of bottom bar when pressed

--- mobile_app/lib/widgets/bottom_bar.dart
+++ mobile_app/lib/widgets/bottom_bar.dart
@@ -9,32 +9,40 @@ class CustomBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> iconPaths = [
+      'lib/assets/icons/home-filled.svg',
+      'lib/assets/icons/MedCat.svg',
+      'lib/assets/icons/map-location.svg',
+      'lib/assets/icons/activity-waves.svg',
+      'lib/assets/icons/profile-round.svg',
+    ];
+
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
       items: [
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset('lib/assets/icons/home-filled.svg', height: 35, width: 35),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset('lib/assets/icons/MedCat.svg', height: 35, width: 35),
-          label: 'MedCat',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset('lib/assets/icons/map-location.svg', height: 35, width: 35),
-          label: 'Maps',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset('lib/assets/icons/activity-waves.svg', height: 35, width: 35),
-          label: 'Activities',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset('lib/assets/icons/profile-round.svg', height: 30, width: 30),
-          label: 'Profile',
-        ),
+        _buildNavigationBarItem(iconPaths[0], 'Home', 0),
+        _buildNavigationBarItem(iconPaths[1], 'MedCat', 1),
+        _buildNavigationBarItem(iconPaths[2], 'Maps', 2),
+        _buildNavigationBarItem(iconPaths[3], 'Activities', 3),
+        _buildNavigationBarItem(iconPaths[4], 'Profile', 4),
       ],
       currentIndex: selectedIndex,
       onTap: (index) => handleBarTapped(context, index),
+    );
+  }
+
+  BottomNavigationBarItem _buildNavigationBarItem(String iconPath, String label, int index) {
+    String modifiedIconPath = index == selectedIndex ? iconPath.replaceFirst('.svg', '-orange.svg') : iconPath;
+
+    double iconSize = index == 4 ? 30 : 35.0;
+
+    return BottomNavigationBarItem(
+      icon: SvgPicture.asset(
+        modifiedIconPath,
+        height: iconSize,
+        width: iconSize,
+      ),
+      label: label,
     );
   }
 }
